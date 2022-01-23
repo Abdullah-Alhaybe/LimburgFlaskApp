94aeccee4f7fd7fe34b539ec4c62df0612d96ebb
Abdullah Haybe | DAAT <abdullah@daatonderzoek.nl>
2022-01-23 21:51:45 +0100

    testing

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ users.append(User(id=1,username='admin',password='admin'))
 
 
 
-@app.route('/login',methods = ['POST','GET'])
+@app.route('/',methods = ['POST','GET'])
 def login():
     '''Returns a static HTML page'''
     if request.method=='POST':
